e3059c91475551597e50f2435dcbc5a036d1dc9f
Jacek Jakubik <jakubik.jacek@gmail.com>
2013-03-24 19:03:38 +0100

    player password

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,15 +1,23 @@
+require 'digest/sha1'
+
 class Player
   include Mongoid::Document
   include Mongoid::Timestamps
 
   field :name, type: String
+  field :password, type: String
+
   belongs_to :account
   has_one :entry, autobuild: true
 
-  attr_accessible :name, :entry_attributes
+  attr_accessible :name, :password, :entry_attributes
 
-  validates_presence_of :name, :entry, :account
+  validates_presence_of :name, :entry, :account, :password
   validates_uniqueness_of :name
 
   accepts_nested_attributes_for :entry
+
+  def password=(value)
+    self.write_attribute(:password, Digest::SHA1.hexdigest(value))
+  end
 end
